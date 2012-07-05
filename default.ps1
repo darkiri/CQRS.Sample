f94f1c96835e5922ce6e65ab3aaab5d885b0add1
darkiri <darkiri@gmail.com>
2012-07-05 09:08:50 +0200

    build target vor tests

--- default.ps1
+++ default.ps1
@@ -1,21 +1,23 @@
 properties {
    $sln = "CQRS.Sample.sln"
+   $config = "Release"
+   $mspec = "packages\Machine.Specifications.0.5.7-beta4\tools\mspec-x86-clr4.exe"
 }
 
 task default -depends Test
 
-task Test -depends Compile, Clean { 
-
+task Test -depends Compile, Clean {
+   Exec { &$mspec "Tests\bin\$config\CQRS.Sample.Tests.dll" } 
 }
 
 task Compile -depends Clean { 
    Get-ChildItem -i packages.config -rec | %{ .\tools\NuGet\NuGet.exe install $_ -OutputDirectory packages }
 
-   Exec{ msbuild /verbosity:m /t:Build /p:Configuration=Release $sln }   
+   Exec { msbuild /verbosity:m /t:Build /p:Configuration=$config $sln }   
 }
 
 task Clean { 
-   Exec{ msbuild /verbosity:m /t:Clean $sln }   
+   Exec { msbuild /verbosity:m /t:Clean $sln }   
 }
 
 task ? -Description "Helper to display task info" {
